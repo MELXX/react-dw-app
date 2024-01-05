2b47e516593f3908aede2eecb18f9fb693b9f08d
kmgwenya <kmgwenya@retrorabbit.co.za>
2024-01-06 00:07:36 +0200

    work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import week1 from './data.json'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Current list for {Date.now()}</h1>
+        {
+            fetch("./sample.json") 
+                .then((res) => { 
+                return res.json(); 
+            }) 
+            .then((data) => <p>{data}</p>)
+        
+          }
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
