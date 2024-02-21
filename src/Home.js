a0259bdcc8fe800e211c284aad950d8efb6ffa11
kmgwenya <kmgwenya@retrorabbit.co.za>
2024-02-21 18:06:05 +0200

    state init

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,23 @@
 import { Link } from 'react-router-dom';
+import { getUsers } from './helpers/getData';
+import { useState,useEffect } from 'react';
 
  function Home(){
-    
+    const [data,dataChanger] = useState([{name:""}]);
+
+    useEffect(() => {
+      getUsers().then(response => {
+          dataChanger(response);
+        })
+        .catch(error => {
+          console.error(error);
+        });  
+    }, []);
+    console.log(data)
     return (
         <div>
-            <h1>Current roster</h1>
-            <div>
-                <p>Monday: Melusi</p>
-                <p>Monday: Melusi</p>
-                <p>Monday: Melusi</p>
-                <p>Monday: Melusi</p>
-                <p>Monday: Melusi</p>
-                <p>Monday: Melusi</p>
-            </div>
+            <h1>Current Users</h1>
+            {data.map((x)=><p key={x.name}>name: {x.name}</p>)}
             <button><Link to="/edit">Edit roster</Link></button>
             <button><Link to="/contact">Contact admin</Link></button>
         </div>
---
+++ src/Notification.jsx
@@ -0,0 +1,9 @@
+import { Children } from "react";
+
+export default function Notification({children}){
+    return (
+        <div>
+            <h1>This is the notification component</h1>
+        </div>
+    );
+}
